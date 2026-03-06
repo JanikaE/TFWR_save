72756ec121d7881e22182ced131579eaabd716ad
zhjy <2272253314@qq.com>
2026-03-06 18:01:45 +0800

    添加多无人机Sunflower

--- Sunflower.py
+++ Sunflower.py
@@ -1,11 +1,12 @@
 import Helper
 n = get_world_size()
 
-def Sunflower():
+# 排序_单无人机
+def Sort():
 	list = []
 	list.insert(0, (-1,-1,0))
-	for i in range(n):
-		for j in range(n):
+	for _ in range(n):
+		for _ in range(n):
 			x = get_pos_x()
 			y = get_pos_y()
 			if (get_ground_type() == Grounds.Grassland):
@@ -26,4 +27,41 @@ def Sunflower():
 		Helper.move_to(pos[0], pos[1])
 		list.pop(index)
 		harvest()
-		
+
+# 全7_32无人机
+def AllSeven_M():
+	def Plant():
+		for _ in range(n):
+			if (get_ground_type() == Grounds.Grassland):
+				till()
+			while get_water() < 0.75:
+				use_item(Items.Water)
+			while measure() != 7:
+				harvest()
+				plant(Entities.Sunflower)
+			move(North)
+	def Harvest():
+		for _ in range(n):
+			harvest()
+			move(North)
+	while True:
+		# Plant
+		Helper.move_to(0, 0)
+		finishList = []
+		for _ in range(n-1):
+			finishList.append(spawn_drone(Plant))
+			move(East)
+		Plant()
+		for i in range(n-1):
+			if (finishList[i] != None):
+				wait_for(finishList[i])
+		# Harvest
+		Helper.move_to(0, 0)
+		finishList = []
+		for _ in range(n-1):
+			finishList.append(spawn_drone(Harvest))
+			move(East)
+		Harvest()
+		for i in range(n-1):
+			if (finishList[i] != None):
+				wait_for(finishList[i])
--- f0.py
+++ f0.py
@@ -1,4 +1,5 @@
 import Helper
 harvest()
+pet_the_piggy()
 #Helper.clear_and_till()
 clear()
